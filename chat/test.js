ff497dbabf13c65e02621b9533166b64fe8c0ede
bit-jax <axel.omley@gmail.com>
2021-04-16 21:07:17 -0700

    w2d5

--- chat/test.js
+++ chat/test.js
@@ -1,8 +1,29 @@
-const http = require('http')
+// const http = require('http')
 
-const iUrl = '/messages'
+const { join } = require("node:path")
 
-const server = http.createServer(function (request, response) {
-    const myPath = new URL (`http://localhost:8000${iUrl}`
-    )
-})
+// const iUrl = '/messages'
+
+// const server = http.createServer(function (request, response) {
+//     const myPath = new URL (`http://localhost:8000${iUrl}`
+//     )
+// })
+
+
+
+// const dummy = ''
+
+// format = username,message
+// post
+// dummy.join('jo,this is a message\n')
+
+
+// get
+// dummy.split('\n') // [(jo,this is a message), (bob,this is also a message)]
+// for i in dummy:
+//     i.split(',') [[jo, this is a message], [bob, this is also a message]]
+//     console.log(i[0]) //jo
+//     console.log(i[1]) // this is a message
+
+
+//     ===
